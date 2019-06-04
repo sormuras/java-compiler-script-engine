061fc456211465f78b1b12e8d618fab9b7a74260
Christian Stein <sormuras@gmail.com>
2019-06-04 18:13:12 +0200

    Format

--- src/main/de.sormuras.javacompilerscriptengine/module-info.java
+++ src/main/de.sormuras.javacompilerscriptengine/module-info.java
@@ -1,7 +1,10 @@
+/**
+ * Provides the Java Compiler Script Engine factory.
+ */
 module de.sormuras.javacompilerscriptengine {
   requires java.scripting;
   requires jdk.compiler;
 
-  provides javax.script.ScriptEngineFactory
-      with de.sormuras.javacompilerscriptengine.JavaCompilerScriptEngineFactory;
+  provides javax.script.ScriptEngineFactory with
+      de.sormuras.javacompilerscriptengine.JavaCompilerScriptEngineFactory;
 }
